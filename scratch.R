8e63c91ab840a601b7cf69cba599ddfc45dd4d34
Melissa Gayton <melissa@Melissas-MacBook-Pro-3.local>
2018-11-07 14:10:45 -0500

    first commit

--- scratch.R
+++ scratch.R
@@ -10,18 +10,34 @@ library(fs)
 
 # 1. Read data/ex_926_I.csv into a tibble and provide a summary.
 
+ex_926<- read_csv("data/ex_926_I.csv")
+summary(ex_926)
 # 2. Create a vector with all the file names in data/.
 
+file_names <- dir_ls("data/")
+
 # 3. Create a vector with just the file names that have an "A" in them.
+
+file_names_A <- str_subset(file_names, pattern = "A")
 
 # 4. Read in all the files into one big tibble. Check out ?map_dfr . . .
 # Background reading here:
 # https://r4ds.had.co.nz/iteration.html#the-map-functions
 
+all <- map_dfr(file_names, read_csv)
+
 # 5. Read in everything and also add a new variable, source, which records the
 # file name from which the data came.
 
+all_edit <- map_dfr(file_names, read_csv, .id  = "source")
+
 # 6. Find the 4 files with the largest number of observations.
+
+largest <- all_edit %>%
+  group_by(source) %>%
+  count() %>%
+  arrange(desc(n)) %>%
+  head(4)
 
 # 7. Write a function which takes a character string like "A" and then reads in
 # all the files which have "A" in the name.
